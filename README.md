b32efcb7bfac4c1ea0d56852f274deae9c32c423
siro844 <140879194+siro844@users.noreply.github.com>
2023-12-30 19:55:45 +0530

    new

--- README.md
+++
@@ -1,1 +0,0 @@
-# Conversational_Interface
--- android/app/src/main/kotlin/com/example/conversational_interface/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.conversational_interface
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
